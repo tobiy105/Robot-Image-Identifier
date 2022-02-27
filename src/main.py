172f0d3b40fa62547a0ff2102a790738822db9ed
Rafael Papallas <rpapallas@gmail.com>
2022-02-27 20:36:25 +0000

    Initial commit.

---
+++ src/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import rospy
+
+if __name__ == '__main__':
+    # Your code should go here. You can break your code into several files and
+    # include them in this file. You just need to make sure that your solution 
+    # can run by just running rosrun group_project main.py
+    pass
